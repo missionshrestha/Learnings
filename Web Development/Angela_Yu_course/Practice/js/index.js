43f384f6d862c8f2487ffe6b88f23085d6cf2d5f
Mission Shretha <missionshrestha99@gmail.com>
2022-07-11 07:37:36 +0545

    Practice change 8

---
+++ Web Development/Angela_Yu_course/Practice/js/index.js
@@ -0,0 +1,7 @@
+document.querySelector("h1").innerHTML="AJSLA";
+
+document.querySelector("a").style.color='red';
+// document.querySelector("h1").style.fontSize='5rem';
+// document.querySelector("h1").ontouchmove=style.fontSize='5rem';
+
+document.querySelector("button").style.backgroundColor='yellow';
