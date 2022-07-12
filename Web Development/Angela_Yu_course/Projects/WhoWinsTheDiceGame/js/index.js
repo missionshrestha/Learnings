be91ea0985cc2d41e58a42b95706c81a63e51908
Mission Shretha <missionshrestha99@gmail.com>
2022-07-12 07:21:15 +0545

    Dice Game Made + Initial Commit

---
+++ Web Development/Angela_Yu_course/Projects/WhoWinsTheDiceGame/js/index.js
@@ -0,0 +1,80 @@
+// Method 1
+
+function RandomDiceGenerator() {
+    var ranNum1 = Math.random();
+    var ranNum2 = Math.random();
+    ranNum1 = Math.floor(ranNum1 * 6 + 1);
+    ranNum2 = Math.floor(ranNum2 * 6 + 1);
+
+    console.log(ranNum1);
+    console.log(ranNum2);
+
+    // document.querySelector(".Dice-Image-1").innerHTML = (` <img src="/WhoWinsTheDiceGame/assets/Images/dice${ranNum1}.png" alt="" />`);
+    // document.querySelector(".Dice-Image-1").innerHTML = ' <img src="/WhoWinsTheDiceGame/assets/Images/dice${ranNum1}.png" alt="" />';
+
+    // document.querySelector(".Dice-Image-2").innerHTML = (` <img src="/WhoWinsTheDiceGame/assets/Images/dice${ranNum2}.png" alt="" />`);
+
+    document.querySelector(".Dice-Image-1 img").setAttribute("src", (`/WhoWinsTheDiceGame/assets/Images/dice${ranNum1}.png`));
+    // document.querySelector(".Dice-Image-1 img").setAttribute("src", '/WhoWinsTheDiceGame/assets/Images/dice${ranNum1}.png');
+
+    document.querySelector(".Dice-Image-2 img").setAttribute("src", (`/WhoWinsTheDiceGame/assets/Images/dice${ranNum2}.png`));
+
+
+    if (ranNum1 > ranNum2) {
+        document.querySelector("h1").innerHTML = "Player 1 wins";
+    }
+    else if (ranNum1 === ranNum2) {
+        document.querySelector("h1").innerHTML = "It's a Draw";
+
+    }
+    else {
+        document.querySelector("h1").innerHTML = "Player 2 wins";
+
+
+    }
+
+}
+RandomDiceGenerator();
+
+
+// Methods 2;
+/* 
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "/WhoWinsTheDiceGame/assets/Images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];//0 for 1st image
+
+image1.setAttribute("src", randomImageSource);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "/WhoWinsTheDiceGame/assets/Images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];//1 for 2nd image
+
+image2.setAttribute("src", randomImageSource2);
+
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+}
+else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "It's a Draw";
+
+}
+else {
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+
+
+}
+
+
+ */
