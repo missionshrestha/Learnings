cb388644b5d427925f77295dbb6dd0511d108435
Mission Shretha <missionshrestha99@gmail.com>
2022-07-18 14:58:50 +0545

    Practice Change 13

--- Web Development/Angela_Yu_course/Practice/js/index.js
+++ Web Development/Angela_Yu_course/Practice/js/index.js
@@ -1,70 +1,24 @@
 
-var numberOfDrimButtons = document.querySelectorAll(".drum").length
 
 
-for (let index = 0; index < numberOfDrimButtons; index++) {
-    document.querySelectorAll(".drum")[index].addEventListener("click", function () {
+//using keypress in jQuery
+/* $("input").keypress(function (e) {
+    console.log(e.key);
+    $("h1").text(e.key);
+});
 
-        var buttonInnerHtml = this.innerHTML;
-        selectSound(buttonInnerHtml);
-        buttonAnimation(buttonInnerHtml);
-    });
-}
+$("h1").mouseover(function () {
+    $("h1").css("color", "purple");
 
-document.addEventListener("keypress", function (event) {
-    selectSound(event.key);
-    buttonAnimation(event.key);
+});
 
-})
+$("h1").on("mouseover", function () {
+    $("h1").css("color", "red");
 
-function selectSound(key) {
-
-    switch (key) {
-        case "w":
-            var audio = new Audio('/Web Development/Angela_Yu_course/Practice/assets/sounds/tom-1.mp3');
-            audio.play();
-            break;
-        case "a":
-            var audio = new Audio('/Web Development/Angela_Yu_course/Practice/assets/sounds/tom-2.mp3');
-            audio.play();
-            break;
-        case "s":
-            var audio = new Audio('/Web Development/Angela_Yu_course/Practice/assets/sounds/tom-3.mp3');
-            audio.play();
-            break;
-        case "d":
-            var audio = new Audio('/Web Development/Angela_Yu_course/Practice/assets/sounds/tom-4.mp3');
-            audio.play();
-            break;
-        case "j":
-            var audio = new Audio('/Web Development/Angela_Yu_course/Practice/assets/sounds/snare.mp3');
-            audio.play();
-            break;
-        case "k":
-            var audio = new Audio('/Web Development/Angela_Yu_course/Practice/assets/sounds/crash.mp3');
-            audio.play();
-            break;
-        case "l":
-            var audio = new Audio('/Web Development/Angela_Yu_course/Practice/assets/sounds/kick-bass.mp3');
-            audio.play();
-            break;
-
-        default:
-            console.log(key)
-            break;
-    }
-
-}
+}); */
 
 
-function buttonAnimation(currentKey) {
-    console.log(currentKey)
-    var activeButton = document.querySelector("." + currentKey);
-    activeButton.classList.add("pressed");
+$("button").on("click", function () {
+    $("h1").slideUp().slideDown().animate({ opacity: .5 });
 
-    setTimeout(function () {
-        activeButton.classList.remove("pressed")
-    }, 200);
-}
-
-
+});
