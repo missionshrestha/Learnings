899a670589dd0d776785b1e774109b219a24ccc0
Mission Shretha <missionshrestha99@gmail.com>
2022-09-17 17:12:13 +0545

    Practice change 15

--- Web Development/Angela_Yu_course/Practice/my-exress-server/server.js
+++ Web Development/Angela_Yu_course/Practice/my-exress-server/server.js
@@ -8,6 +8,19 @@ app.get("/", function (request, response) {
     response.send("Hello");
 });
 
+app.get("/contact", function (req, res) {
+    res.send("This is contact page");
+});
+
+app.get("/about", function (req, res) {
+    res.send("Hi my name is mission shrestha");
+});
+
+app.get("/hobbies", function (req, res) {
+    res.send("These are my hobbies");
+
+});
+
 
 app.listen(port, function () {
     console.log("Server has started at port 3000");
