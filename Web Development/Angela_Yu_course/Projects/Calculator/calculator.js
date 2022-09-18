74d581ed9fab3437f02d4cd7280c154300813f48
Mission Shretha <missionshrestha99@gmail.com>
2022-09-18 10:15:53 +0545

    Calculator Project change 2

--- Web Development/Angela_Yu_course/Projects/Calculator/calculator.js
+++ Web Development/Angela_Yu_course/Projects/Calculator/calculator.js
@@ -1,21 +1,35 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 1010;
+
 
 app.listen(port, function () {
     console.log(` Example app listening on port ${port} `);
 })
 
 app.get("/", function (req, res) {
-    res.send(
-        "This is the Home page!"
-    )
+    res.sendFile(__dirname + "/index.html");
 });
+
 app.get("/calculator", function (req, res) {
     res.send(
         "<h1>Welcome to my calculator!</h1>"
-    )
+    );
 });
+
+
+//this helps to response(to / location) to post from html form
+app.post("/", function (req, res) {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send(`The sum of 2 is: ${result}`);
+
+
+})
+
+
 
