3555006858846215183d9052c3479e85bc819fc0
Mission Shretha <missionshrestha99@gmail.com>
2022-11-07 07:21:08 +0545

    ToDoList Project Change 1: Listing of new ToDoItem implemented

--- Web Development/Angela_Yu_course/Projects/ToDoList-v1/app.js
+++ Web Development/Angela_Yu_course/Projects/ToDoList-v1/app.js
@@ -4,48 +4,46 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var addedItemArray = ["First Task"];//will be used in both app.get & app.post
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
 
     var today = new Date();
-    const currentDay = today.getDay();
-    var day = '';
-    console.log(currentDay);
-
-    switch (currentDay) {
-        case 0:
-            day = 'Sunday'
-            break;
-        case 1:
-            day = 'Monday'
-            break;
-        case 2:
-            day = 'Tuesday'
-            break;
-        case 3:
-            day = 'Wednesday'
-            break;
-        case 4:
-            day = 'Thursday'
-            break;
-        case 5:
-            day = 'Friday'
-            break;
-        case 6:
-            day = 'Saturday'
-            break;
-
-        default:
-            day = 'This is not actual day'
-            console.log("Error! Current Day is: " + currentDay)
-            break;
-    }
-
+    var options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    var date = today.toLocaleDateString("en-US", options); // Saturday, September 17, 2016
 
     //this list file must be inside views for render to work
-    res.render('list', { kindOfDay: day })
+    res.render(
+        'list',
+        {
+            kindOfDay: date,
+            newItemListArray: addedItemArray
+        });
+    //we pass date,addedItem to kindOfDay, newListItem present in list.ejs
+});
+
+app.post("/", function (req, res) {
+
+    addedItem = req.body.newItem;//must use app.use(bodyParser.urlencoded({ extended: true }));
+    console.log(addedItem);
+    addedItemArray.push(addedItem);
+    res.redirect("/");
+    //when post request is triggered, we redirect to trigger app.get()
+
+    /* res.render("list", { newListItem: addedItem }); */
+    //we cannot render this bcoz each time we render / home route, 
+    // new app.get() is triggered & there we only pass kindOfDay:date but not newListItem
+
+
+
 });
 
 app.listen(3000, function () {
