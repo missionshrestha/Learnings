4efb5247e90e8dfd872a74854b5a3fef74e14c10
Mission Shretha <missionshrestha99@gmail.com>
2022-11-16 06:53:10 +0545

    add work,about page with impementation of common header & footer

--- Web Development/Angela_Yu_course/Projects/ToDoList-v1/app.js
+++ Web Development/Angela_Yu_course/Projects/ToDoList-v1/app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 let addedItemArray = ["First Task"];//will be used in both app.get & app.post
-
+let workItemArray = ["Work 1"];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -26,27 +26,57 @@ app.get("/", function (req, res) {
     res.render(
         'list',
         {
-            kindOfDay: date,
+            listTitle: date + " Home ",
             newItemListArray: addedItemArray
         });
     //we pass date,addedItem to kindOfDay, newListItem present in list.ejs
 });
 
 app.post("/", function (req, res) {
-
+console.log(req.body)
     addedItem = req.body.newItem;//must use app.use(bodyParser.urlencoded({ extended: true }));
     console.log(addedItem);
-    addedItemArray.push(addedItem);
-    res.redirect("/");
-    //when post request is triggered, we redirect to trigger app.get()
+    if (req.body.list == "Work List") {
+        workItemArray.push(addedItem);
+        res.redirect("/work")
+    }
+    else {
+        addedItemArray.push(addedItem);
+        res.redirect("/");
+        //when post request is triggered, we redirect to trigger app.get()
 
-    /* res.render("list", { newListItem: addedItem }); */
-    //we cannot render this bcoz each time we render / home route, 
-    // new app.get() is triggered & there we only pass kindOfDay:date but not newListItem
+        /* res.render("list", { newListItem: addedItem }); */
+        //we cannot render this bcoz each time we render / home route, 
+        // new app.get() is triggered & there we only pass kindOfDay:date but not newListItem
+
+    }
 
 
 
 });
+
+app.get("/work", function (req, res) {
+    res.render(
+        'list',
+        {
+            listTitle: "Work List",
+            newItemListArray: workItemArray
+        }
+    )
+});
+
+/* app.post('/work', function (req, res) {
+    let addedWorkItem = req.body.newItem;
+    workItemArray.push(addedWorkItem);
+    res.redirect("/work");
+}); */
+
+app.get('/about',function (req,res) {
+    res.render(
+        'about'
+    )
+});
+
 
 app.listen(3000, function () {
     console.log("Server started at port 3000.")
