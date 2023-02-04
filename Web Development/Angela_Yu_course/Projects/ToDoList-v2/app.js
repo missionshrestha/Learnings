3bc982459cf04ab5fb82fedacbfc716ec732a26a
Mission Shretha <missionshrestha99@gmail.com>
2023-02-04 12:33:14 +0545

    Todos List v2 completed

--- Web Development/Angela_Yu_course/Projects/ToDoList-v2/app.js
+++ Web Development/Angela_Yu_course/Projects/ToDoList-v2/app.js
@@ -2,38 +2,167 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
+
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB")
+
+const itemSchema = {
+  name: {
+    type: String,
+    required: [true, "Must enter the item name"]
+  }
+}
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Buy Food"
+});
+const item2 = new Item({
+  name: "Cook Food"
+});
+const item3 = new Item({
+  name: "Eat Food"
+});
+
+const defaultItems = [item1, item2, item3];
+
+//creating a listSchema having list title & array of items
+const listSchema = {
+  name: String,
+  items: [itemSchema]
+}
+
+const List = mongoose.model("List", listSchema);
+
+
 
 app.get("/", function (req, res) {
 
-  const day = date.getDate();
+  Item.find({},
+    function (err, foundItems) {
+      if (err) {
+        console.log(err);
+        console.log("Error found in find Item");
+      }
+      else {
+        if (foundItems.length === 0) {
+          Item.insertMany(defaultItems, function (err) {
+            if (err) {
+              console.log(err);
+              console.log("Error found in insertMany");
+            }
+            else {
+              console.log("Successfully inserted defaultItems")
+            }
+          })
 
-  res.render("list", { listTitle: day, newListItems: items });
+          res.redirect("/")
+        }
+        else {
+          console.log(foundItems)
+          console.log("Successfully found Items")
+          res.render("list", { listTitle: "Today", newListItems: foundItems });
+        }
+      }
+    })
+
+});
+
+app.get("/:customListName", function (req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+
+
+  List.findOne({ name: customListName }, function (err, foundlist) {
+    if (!err) {
+      if (!foundlist) {
+        //Create new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        })
+        list.save();
+        res.redirect(`/${customListName}`);
+      }
+      else {
+        // show existing list
+        res.render("list", { listTitle: foundlist.name, newListItems: foundlist.items });
+
+      }
+    }
+  })
+
 
 });
 
 app.post("/", function (req, res) {
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
+
+  const item = new Item({
+    name: itemName
+  });
+
+  if (listName === "Today") {
+    item.save();
+    res.redirect("/")
   }
+  else {
+    List.findOne({ name: listName }, function (err, foundList) {
+      foundList.items.push(item);
+      foundList.save()
+      res.redirect(`/${listName}`)
+    })
+
+  }
+
+
 });
+
+app.post("/delete", function (req, res) {
+  const toBeDeletedItem = req.body.checkedItem.trim()
+  //trim should be used to remove unwanted spaces in string
+  const listName = req.body.listName.trim();
+  console.log(listName);
+  if (listName === "Today") {
+    Item.findByIdAndRemove(toBeDeletedItem, function (err) {
+      if (err) {
+        console.log(err);
+        console.log("Error found in delete");
+      }
+      else {
+
+        console.log("Successfully deleted item")
+        res.redirect("/")
+
+      }
+    })
+  }
+
+  else {
+
+    List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: toBeDeletedItem } } }, function (err, foundList) {
+      if (!err) {
+        res.redirect(`/${listName}`)
+      }
+    });
+
+  }
+
+
+
+})
 
 app.get("/work", function (req, res) {
   res.render("list", { listTitle: "Work List", newListItems: workItems });
